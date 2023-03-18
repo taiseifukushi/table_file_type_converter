c0736a4f2222813ce230ed497f0f593e7c038b8c
husita-h <taisei.fukushi1129@gmail.com>
2023-03-18 19:41:58 +0900

    fix: ヘッダーとそれ以外を区切る「---」の行を取り除く(#2)

--- public/js/main.js
+++ public/js/main.js
@@ -7,57 +7,39 @@ const excelDownloadLink = document.getElementById("excel-download-link");
 
 convertButton.addEventListener("click", () => {
 	const file = fileInput.files[0];
-	const reader = new FileReader(); // FileReader: https://developer.mozilla.org/ja/docs/Web/API/FileReader
+	const reader = new FileReader();
 	reader.onload = function () {
-		const markdownTable = reader.result; // reader.onload: FileReaderがファイルの読み込み(`reader.readAsText(file)`)を完了したときに呼び出されるコールバック関数
-		const rowPattern = /^\|(.+)\|$/; // 先頭が|で、末尾が|である1行の文字列 = 区切り線ではない行
-		const separatorPattern = /^\|(-+)\|$/; // 先頭が|で、末尾が|である一つ以上の`-`を含む文字列 = 区切り線の行
-		let isHeaderRow = true; // 処理中の行がmdテーブルのヘッダーかどうかを表す
+		const markdownTable = reader.result;
+		const rowPattern = new RegExp(/^\|(\s*[\w\s]+\s*\|)+$/);
+		const separatorPattern = new RegExp(/^\|(\s*[-:]+\s*\|)+$/);
 		const tableData = [];
 		const rows = markdownTable.split("\n");
 
-		// test: https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/RegExp/test
 		for (const row of rows) {
 			if (separatorPattern.test(row)) {
-				isHeaderRow = false;
+				console.log("separatorPattern", row);
 				continue;
 			}
-
+			
 			if (rowPattern.test(row)) {
-				const rowData = row
-					.trim() // " | aaa | bbb | ccc | ddd |  " => "| aaa | bbb | ccc | ddd |"
-					.slice(1, -1) // "| aaa | bbb | ccc | ddd |" => " aaa | bbb | ccc | ddd "
-					.split("|") // " aaa | bbb | ccc | ddd " => [' aaa ', ' bbb ', ' ccc ', ' ddd ']
-					.map((cell) => cell.trim()); // [' aaa ', ' bbb ', ' ccc ', ' ddd '] => ['aaa', 'bbb', 'ccc', 'ddd']
-
-				// 区切り行の場合
-				if (isHeaderRow) {
-					const headerData = rowData.map((cell) => ({
-						value: cell,
-						bold: true,
-					}));
-					tableData.push(headerData);
-				} else {
-					const dataRow = rowData.map((cell) => ({
-						value: cell,
-					}));
-					tableData.push(dataRow);
-				}
-				isHeaderRow = false; // 最初の行を処理した後フラグをリセットする
+				console.log("rowPattern", row);
+				const rowData = row.trim().slice(1, -1).split("|").map((cell) => cell.trim());
+				const dataRow = rowData.map((cell) => ({
+					value: cell,
+				}));
+				tableData.push(dataRow);
 			}
 		}
 
-		// CSVファイルの作成
 		const csvData = tableData
 			.map((row) => row.map((cell) => cell.value).join(","))
 			.join("\n");
-		const csvBlob = new Blob([csvData], { type: "text/csv" }); // https://developer.mozilla.org/ja/docs/Web/API/Blob
-		csvDownloadLink.href = URL.createObjectURL(csvBlob); // https://developer.mozilla.org/ja/docs/Web/API/URL/createObjectURL
+		const csvBlob = new Blob([csvData], { type: "text/csv" });
+		csvDownloadLink.href = URL.createObjectURL(csvBlob);
 		csvDownloadLink.download = "table.csv";
     csvTableContainer.innerHTML =
 				"The conversion to an CSV file is complete.";
 
-		// Excelファイルの作成
 		const workbook = XLSX.utils.book_new();
 		const worksheet = XLSX.utils.aoa_to_sheet(
 			tableData.map((row) => row.map((cell) => cell.value))
@@ -67,12 +49,12 @@ convertButton.addEventListener("click", () => {
 			[
 				s2ab(
 					XLSX.write(workbook, {
-						type: "binary", // 出力をバイナリデータにする
+						type: "binary",
 					})
 				),
 			],
 			{
-				type: "application/octet-stream", // ExcelファイルのMIMEタイプ
+				type: "application/octet-stream",
 			}
 		);
 		excelDownloadLink.href = URL.createObjectURL(excelBlob);
@@ -83,11 +65,6 @@ convertButton.addEventListener("click", () => {
   reader.readAsText(file);
 });
 
-// 文字列をバイナリデータに変換する
-// https://github.com/SheetJS/sheetjs/blob/master/dist/xlsx.full.min.js#L9
-// https://qiita.com/tomgoodsun/items/0107e5d778b803935fc0
-// https://docs.sheetjs.com/docs/demos/local/file#binary-data
-// https://docs.sheetjs.com/docs/api/parse-options#guessing-file-type
 function s2ab(s) {
   const buf = new ArrayBuffer(s.length);
   const view = new Uint8Array(buf);
